fdfb72b399e390a424b4e38e26a77fe1943c5e59
unknown <irahavoi@gmail.com>
2015-06-08 09:53:56 -0500

    Up

--- js/app/hello.js
+++ js/app/hello.js
@@ -5,13 +5,42 @@ var myAppModule = angular.module('myApp', []);
 myAppModule.controller('HelloWorldController', function($scope){
     $scope.updateChangeCounter = function(){
         $scope.changesCount++;
+
+        if($scope.changesCount > 10){
+            $scope.dereg();
+            $scope.result = "Too many changes! X_X";
+        }
     }
 
-    $scope.$watch('result', $scope.updateChangeCounter);
+
     $scope.reset = function(){
+        $scope.dereg = $scope.$watch('result', $scope.updateChangeCounter);
         $scope.result = 'Hello, Angular!';
         $scope.changesCount = 0;
     }
 
     $scope.reset();
+
+    $scope.students = [
+        {
+            name : 'James Bond',
+            id : '1'
+        },
+        {
+            name : 'Gandalf White',
+            id : '2'
+        },
+        {
+            name : 'Marry Poppins',
+            id : '3'
+        }
+    ];
+
+    $scope.addNewStudent = function(){
+        $scope.students.push({name: 'NewStudent', id : 'x'});
+    }
+
+    $scope.removeLastStudent = function(){
+        $scope.students.pop();
+    }
 });
