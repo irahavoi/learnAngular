79b9d6da5bb36c7fc204a2769776095787b5003e
unknown <irahavoi@gmail.com>
2015-06-09 09:20:38 -0500

    Up

--- js/app/customDirective.js
+++ js/app/customDirective.js
@@ -3,6 +3,12 @@
  */
 var myModule = angular.module('myApp', []);
 
+//If you want the dynamically-loaded template to be available before the directive run, call it via a run function:
+myModule.run(function($templateCache, $http){
+   var template =  $http.get('helloCachedTemplate.html');
+   $templateCache.put('helloCachedTemplate.html', template);
+});
+
 
 myModule.directive('hello', function(){
     return {
@@ -15,7 +21,30 @@ myModule.directive('hello', function(){
 myModule.directive('bye', function(){
     return {
         restrict: 'E',
-        template : '<div class="alert alert-danger"><strong>Good-Bye!</strong></div>',
+        template : '<div class="alert alert-danger"><strong>Good-Bye! (template is just a string defined as the value of directive\'s "template" property.)</strong></div>',
         replace: true
     }
-})
+});
+
+myModule.directive('seeYa', function(){
+    return {
+        restrict: 'E',
+        templateUrl : 'seeYouTemplateInline.html',
+        replace: true
+    }
+});
+
+myModule.directive('cachedHello', function(){
+    return {
+        restrict: 'E',
+        templateUrl : 'helloCachedTemplate.html',
+        replace: true
+    }
+});
+
+myModule.directive('helloTransclude', function(){
+    return{
+        template : '<div>Hi <strong ng-transclude></strong>. I use transclusion.</div>',
+        transclude : true
+    }
+});
